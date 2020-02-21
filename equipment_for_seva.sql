ef13301036a782a8fbd53bf7a190d0110424bb26
Gokul Krishna <gokulakrishna9@gmail.com>
2020-02-21 14:29:23 +0530

    DB sql

---
+++ equipment_for_seva.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 21, 2020 at 08:57 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `equipment_for_seva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+DROP TABLE IF EXISTS `equipment`;
+CREATE TABLE IF NOT EXISTS `equipment` (
+  `equipment_id` int(6) NOT NULL AUTO_INCREMENT,
+  `equipment_type_id` int(3) NOT NULL,
+  `equipment_procurement_type_id` int(11) NOT NULL,
+  `manufacturer_id` int(11) NOT NULL,
+  `eq_name` varchar(100) NOT NULL,
+  `model` varchar(20) DEFAULT NULL,
+  `serial_number` varchar(100) DEFAULT NULL,
+  `mac_address` varchar(100) NOT NULL,
+  `asset_number` varchar(100) DEFAULT NULL,
+  `donor_id` int(11) NOT NULL,
+  `procured_by_id` int(11) DEFAULT NULL COMMENT 'Owner, map to vendor_id',
+  `purchase_order_date` date DEFAULT NULL,
+  `cost` int(9) DEFAULT NULL,
+  `supplier_id` int(11) NOT NULL COMMENT 'Map to vendor',
+  `invoice_number` int(11) NOT NULL,
+  `invoice_date` date NOT NULL,
+  `supply_date` date DEFAULT NULL,
+  `installation_date` date NOT NULL,
+  `warranty_start_date` date NOT NULL,
+  `warranty_end_date` date NOT NULL,
+  `equipment_status_id` tinyint(1) NOT NULL DEFAULT '1',
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `created_datetime` datetime NOT NULL,
+  `updated_datetime` datetime NOT NULL,
+  PRIMARY KEY (`equipment_id`),
+  KEY `equipment_procurement_type_id` (`equipment_procurement_type_id`),
+  KEY `invoice_number` (`invoice_number`),
+  KEY `donor_id` (`donor_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='List of equipments in hospital';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_accessory`
+--
+
+DROP TABLE IF EXISTS `equipment_accessory`;
+CREATE TABLE IF NOT EXISTS `equipment_accessory` (
+  `equipment_accessory_id` int(11) NOT NULL AUTO_INCREMENT,
+  `equipment_id` int(11) NOT NULL,
+  `accessory_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`equipment_accessory_id`),
+  KEY `equipment_id` (`equipment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_location_log`
+--
+
+DROP TABLE IF EXISTS `equipment_location_log`;
+CREATE TABLE IF NOT EXISTS `equipment_location_log` (
+  `equipment_location_log_id` int(11) NOT NULL,
+  `vendor_id` int(11) NOT NULL,
+  `place_id` int(11) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `delivery_date` date NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `created_datetime` datetime NOT NULL,
+  `updated_datetime` datetime NOT NULL,
+  PRIMARY KEY (`equipment_location_log_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_maintenance_contract`
+--
+
+DROP TABLE IF EXISTS `equipment_maintenance_contract`;
+CREATE TABLE IF NOT EXISTS `equipment_maintenance_contract` (
+  `amc_cmc_id` int(6) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `equipment_id` int(6) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `rate` varchar(10) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `vendor_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Maintenance contracts for equipments';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_procurement_type`
+--
+
+DROP TABLE IF EXISTS `equipment_procurement_type`;
+CREATE TABLE IF NOT EXISTS `equipment_procurement_type` (
+  `equipment_procurement_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `procurment_type` varchar(50) NOT NULL,
+  PRIMARY KEY (`equipment_procurement_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_service_record`
+--
+
+DROP TABLE IF EXISTS `equipment_service_record`;
+CREATE TABLE IF NOT EXISTS `equipment_service_record` (
+  `request_id` int(6) NOT NULL AUTO_INCREMENT,
+  `equipment_id` int(6) NOT NULL,
+  `user_id` int(8) NOT NULL,
+  `call_date` date DEFAULT NULL,
+  `call_time` time DEFAULT NULL,
+  `call_type` varchar(20) NOT NULL COMMENT 'Hardcoded, online, call, inperson',
+  `service_issue_type_id` int(11) NOT NULL,
+  `call_information` varchar(500) DEFAULT NULL,
+  `caller_institution_id` int(10) NOT NULL,
+  `contact_person` varchar(70) NOT NULL,
+  `service_provider_id` varchar(50) DEFAULT NULL COMMENT 'vendor_id',
+  `service_person` varchar(50) DEFAULT NULL,
+  `service_person_phone` varchar(12) NOT NULL,
+  `contact_person_phone` varchar(12) NOT NULL,
+  `service_person_remarks` varchar(500) DEFAULT NULL,
+  `issue_closure` datetime DEFAULT NULL,
+  `working_status_id` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`request_id`),
+  UNIQUE KEY `request_id` (`request_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Service records log for equipments';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_service_record_log`
+--
+
+DROP TABLE IF EXISTS `equipment_service_record_log`;
+CREATE TABLE IF NOT EXISTS `equipment_service_record_log` (
+  `service_record_log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_id` int(11) NOT NULL,
+  `status_note` varchar(500) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  PRIMARY KEY (`service_record_log_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_status_type`
+--
+
+DROP TABLE IF EXISTS `equipment_status_type`;
+CREATE TABLE IF NOT EXISTS `equipment_status_type` (
+  `equipment_status_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `equipment_status_type` varchar(30) NOT NULL,
+  PRIMARY KEY (`equipment_status_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_type`
+--
+
+DROP TABLE IF EXISTS `equipment_type`;
+CREATE TABLE IF NOT EXISTS `equipment_type` (
+  `equipment_type_id` int(3) NOT NULL AUTO_INCREMENT,
+  `equipment_type` varchar(200) NOT NULL,
+  PRIMARY KEY (`equipment_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='List of equipment types';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_working_status`
+--
+
+DROP TABLE IF EXISTS `equipment_working_status`;
+CREATE TABLE IF NOT EXISTS `equipment_working_status` (
+  `working_status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `working_status` varchar(300) NOT NULL,
+  PRIMARY KEY (`working_status_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
